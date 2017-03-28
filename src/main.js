aec66192dd917d6bc0fd84bd2bcbfaf681215bdf
LS <manunited_22@sina.com>
2017-03-28 17:55:43 +0800

    将路由转移到router目录下

--- src/main.js
+++ src/main.js
@@ -2,35 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter);
-
-import storeList from '@/components/commond/storeList'
-
-const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    {
-      path: '/',
-      component: storeList
-    },
-    {
-      path: 'storelist',
-      component: storeList
-    },
-    {
-      path: '*',
-      component: storeList
-    }
-  ]
-})
+import routes from './router/index'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 const app = new Vue({
-  router: router,
+  router: routes,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import storeList from '@/components/commond/storeList'
+
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: storeList
+    },
+    {
+      path: 'storelist',
+      component: storeList
+    },
+    {
+      path: '*',
+      component: storeList
     }
   ]
 })
