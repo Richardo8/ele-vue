a3a027365a6e4f46b14f1bf7b23b242c84088e1d
LS <manunited_22@sina.com>
2017-03-31 17:46:38 +0800

    修复swiper报错，但是依然不能正常使用

--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -4,14 +4,21 @@
     <nav class="msite_nav">
       <div class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide food_types_container" v-for="(item, index) in foodTypes">
-            <a v-for="fooItem in item" class="link_to_food" v-if="foodItem.title !== '预定早餐'">
+          <div class="swiper-slide food_types_container" v-for="(item, index) in foodTypes" :key="index">
+            <a v-for="foodItem in item" class="link_to_food" :key="foodItem.id" v-if="foodItem.title !== '预定早餐'">
               <figure>
-                <img :src="imgBaseUrl + fooItem.image_url">
+                <img :src="imgBaseUrl + foodItem.image_url">
+                <figcaption>{{foodItem.title}}</figcaption>
+              </figure>
+            </a>
+            <a href="https://zaocan.ele.me/" class="link_to_food" v-else>
+              <figure>
+                <img :src="imgBaseUrl + foodItem.image_url">
                 <figcaption>{{foodItem.title}}</figcaption>
               </figure>
             </a>
           </div>
+          <div class="swiper-pagination"></div>
         </div>
       </div>
     </nav>
@@ -31,6 +38,8 @@
   import shopList from '@/components/commond/storeList';
   import {msiteAdress, getFoodTypes} from '@/service/getData';
   import {imgBaseUrl} from '@/config/commonparams/commonData';
+  import '@/plugins/swiper.min.js';
+  import '@/plugins/swiper.min.css';
 
   export default {
       data(){
@@ -47,7 +56,17 @@
       },
       async mounted(){
          let res = await getFoodTypes(this.geohash);
-         this.foodTypes = [...res];
+         let resLength = res.length;
+         let resArr = res.concat([]);
+         let foodArr = [];
+         for(let i = 0, j = 0; i < resLength; i += 8, j++){
+             foodArr[j] = resArr.splice(0, 8);
+         }
+         this.foodTypes = foodArr;
+         let myswiper = new Swiper('.swiper-container', {
+           pagination: '.swiper-pagination',
+           loop: true
+         })
       },
       components: {
           shopList
@@ -57,6 +76,37 @@
 
 <style lang="scss" scoped>
   @import 'src/style/mixin';
+  .msite_nav{
+    padding-top: 2.1rem;
+    background-color: #fff;
+    border-bottom: 0.025rem solid $bc;
+    .swiper-container{
+      @include wh(100%, auto);
+      padding-bottom: 0.6rem;
+      .swiper-pagination{
+        bottom: 0.2rem;
+      }
+    }
+  }
+  .food_types_container{
+    display: flex;
+    flex-wrap: wrap;
+    .link_to_food{
+      width: 25%;
+      padding: 0.3rem 0;
+      @include fj(center);
+      figure{
+        img{
+          margin-bottom: 0.3rem;
+          @include wh(1.8rem, 1.8rem);
+        }
+        figcaption{
+          text-align: center;
+          @include sc(0.55rem, #666);
+        }
+      }
+    }
+  }
   .shop_list_container{
     margin-top: 0.4rem;
     border-top: 0.025rem solid $bc;
