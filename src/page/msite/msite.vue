4b567d69f872882f25cf69df8af85df06f4bde52
LS <manunited_22@sina.com>
2017-04-01 10:44:47 +0800

    添加header组件

--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <span class="title_text ellipsis">{{msiteTitle}}</span>
+    <head-top>
+      <router-link to="" class="link_search" slot="search">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <circle cx="9" cy="9" r="8" stroke="rgb(255,255,255)" stroke-width="2" fill="none"/>
+          <line x1="15" y1="15" x2="20" y2="20" style="stroke:rgb(255,255,255);stroke-width:2"/>
+        </svg>
+      </router-link>
+      <router-link to="" slot="msite-title" class="msite_title">
+        <span class="title_text ellipsis">{{msiteTitle}}</span>
+      </router-link>
+    </head-top>
     <nav class="msite_nav">
       <div class="swiper-container">
         <div class="swiper-wrapper">
@@ -38,6 +48,7 @@
   import shopList from '@/components/commond/storeList';
   import {msiteAdress, getFoodTypes} from '@/service/getData';
   import {imgBaseUrl} from '@/config/commonparams/commonData';
+  import headTop from '@/components/header/header';
   import '@/plugins/swiper.min.js';
   import '@/plugins/swiper.min.css';
 
@@ -69,6 +80,7 @@
          })
       },
       components: {
+          headTop,
           shopList
       }
   }
@@ -76,6 +88,22 @@
 
 <style lang="scss" scoped>
   @import 'src/style/mixin';
+  .link_search{
+    left: 0.8rem;
+    @include wh(1.5rem, 1.5rem);
+    @include ct;
+  }
+  .msite_title{
+    @include center;
+    width: 50%;
+    color: #fff;
+    text-align: center;
+    .title_text{
+      @include sc(0.8rem, #fff);
+      text-align: center;
+      display: block;
+    }
+  }
   .msite_nav{
     padding-top: 2.1rem;
     background-color: #fff;
