4afa1908b8a4e5f17ee0339fd66f0c45e41f4f2d
LS <manunited_22@sina.com>
2017-04-07 11:56:52 +0800

    解决‘image_path’为null的问题

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    123
+  <div v-loading.fullscreen.lock="isLoading" element-loading-text="拼命加载中">
+    <section class="shop_container" v-if="!isLoading">
+      <header class="shop_detail_header" >
+        <img :src="getPicUrl(shopDetailData.image_path)" class="header_cover_img">
+        <section class="description_header">
+          <router-link to="/shop/shopDetail" class="description_top">
+            <section class="description_left">
+              <img :src="getPicUrl(shopDetailData.image_path)">
+            </section>
+          </router-link>
+        </section>
+      </header>
+    </section>
   </div>
 </template>
 
 <script>
 import {mapState, mapMutations} from 'vuex'
+import {getPicUrl} from '@/components/commond/mixins'
 import {msiteAdress, getShopDetails} from '@/service/getData'
 
 export default {
@@ -13,7 +25,8 @@ export default {
         return {
             geohash: '',
             shopId: null,
-            shopDetailData: null
+            shopDetailData: null,
+            isLoading: true
         }
     },
     created(){
@@ -24,6 +37,7 @@ export default {
     mounted(){
         this.initData()
     },
+    mixins: [getPicUrl],
     computed: {
       ...mapState([
           'latitude', 'longitude'
@@ -39,11 +53,21 @@ export default {
                 this.RECORD_ADDRESS(res);
             }
             this.shopDetailData = await getShopDetails(this.shopId, this.latitude, this.longitude)
+            this.isLoading = false;
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import 'src/style/mixin';
 
+.shop_container{
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  right: 0;
+  left: 0;
+  height: 100%;
+}
 </style>
