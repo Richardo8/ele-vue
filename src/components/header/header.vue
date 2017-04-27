aa3567a4cef5e894c58437c734735145c228a309
LS <manunited_22@sina.com>
2017-04-27 16:19:47 +0800

    添加返回按钮

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,11 @@
 <template>
   <header id="head_top">
     <slot name="search"></slot>
+    <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
+      <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2"/>
+      </svg>
+    </section>
     <div class="head_login">
       <span class="login_span">登录|注册</span>
     </div>
@@ -16,7 +21,7 @@
       data(){
           return {}
       },
-      props: ['headTitle']
+      props: ['headTitle', 'goBack']
   }
 </script>
 
